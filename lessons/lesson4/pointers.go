db34c37c7b2c550e05531c3fbbdfd46b9f5eed03
Arseniy Kucher <beeffallo@gmail.com>
2019-08-25 20:01:00 +0300

    Lesson-4

---
+++ lessons/lesson4/pointers.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 15
+	a := &x // memory address
+	fmt.Println(a)
+	fmt.Println(*a)
+
+	*a = 5
+	fmt.Println(x)
+
+	pp := &a
+	fmt.Println(pp)
+	fmt.Println(**pp)
+}
